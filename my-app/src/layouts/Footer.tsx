379918a5eec73d69688e575a412d49abdffdd3f4
hieutrandn9889 <hieutrandn123@gmail.com>
2024-12-29 06:18:55 +0700

    format header and footer

--- my-app/src/layouts/Footer.tsx
+++ my-app/src/layouts/Footer.tsx
@@ -1,10 +1,7 @@
 const Footer = () => {
     return (
-        <div style={{textAlign:'center', padding:'2rem 0', borderTop:'1px solid #F1f6f9'}}>
-            
+        <div style={{ textAlign: 'center', padding: '2rem 0', borderTop: '1px solid #F1f6f9' }}>
             Copyright 2025 - Devloper By HieuTran
-
-
         </div>
     )
 };
--- my-app/src/layouts/Header.tsx
+++ my-app/src/layouts/Header.tsx
@@ -1,11 +1,11 @@
 
 const Header = () => {
-  return (
-    <div className="flex-basic-between" style={{ padding: '1.5rem 4rem', borderBottom:'1px solid #F1f6f9'}}>
-      <div style={{ fontSize: '40px', fontWeight: 600 }}>HieuTranPizza</div>
-      <div className="btn-main">Create Pizza</div>
-    </div>
-  )
+    return (
+        <div className="flex-basic-between" style={{ padding: '1.5rem 4rem', borderBottom: '1px solid #F1f6f9' }}>
+            <div style={{ fontSize: '40px', fontWeight: 600 }}>HieuTranPizza</div>
+            <div className="btn-main">Create Pizza</div>
+        </div>
+    )
 };
 
 export default Header;
