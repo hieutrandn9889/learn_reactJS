f04aa2aab50f98df25d3b273706cee936ba42c56
hieutrandn9889 <hieutrandn123@gmail.com>
2024-12-30 16:13:22 +0700

    type and component

--- my-app/src/pages/HomePage.tsx
+++ my-app/src/pages/HomePage.tsx
@@ -1,68 +1,9 @@
-import LogoPizza from "../assets/pizza.png";
+import CardPizza from "../sections/CardPizza";
 const HomePage = () => {
     return (
         <div style={{ height: 'calc(100vh - 309px)', padding: '4rem 4rem', overflowY: 'auto' }}>
             <div className="wrapper-card-items">
-                <div className="card-items">
-                    <img className="card-image-pizza" src={LogoPizza} alt="9DevPizza" />
-                    <div style={{ height: '50%', width: '100%' }}></div>
-                    <div className="card-content">
-                        <div>
-                            <div style={{ fontSize: '20px', fontWeight: 600, color: '#fff' }}>Pizza Thit Bam</div>
-                            <div style={{ fontSize: '20px', fontWeight: 600, color: '#fff', marginTop: '.5rem' }}>Thit bam, so ca chua</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="card-items">
-                    <img className="card-image-pizza" src={LogoPizza} alt="9DevPizza" />
-                    <div style={{ height: '50%', width: '100%' }}></div>
-                    <div className="card-content">
-                        <div>
-                            <div style={{ fontSize: '20px', fontWeight: 600, color: '#fff' }}>Pizza Thit Bam</div>
-                            <div style={{ fontSize: '20px', fontWeight: 600, color: '#fff', marginTop: '.5rem' }}>Thit bam, so ca chua</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="card-items">
-                    <img className="card-image-pizza" src={LogoPizza} alt="9DevPizza" />
-                    <div style={{ height: '50%', width: '100%' }}></div>
-                    <div className="card-content">
-                        <div>
-                            <div style={{ fontSize: '20px', fontWeight: 600, color: '#fff' }}>Pizza Thit Bam</div>
-                            <div style={{ fontSize: '20px', fontWeight: 600, color: '#fff', marginTop: '.5rem' }}>Thit bam, so ca chua</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="card-items">
-                    <img className="card-image-pizza" src={LogoPizza} alt="9DevPizza" />
-                    <div style={{ height: '50%', width: '100%' }}></div>
-                    <div className="card-content">
-                        <div>
-                            <div style={{ fontSize: '20px', fontWeight: 600, color: '#fff' }}>Pizza Thit Bam</div>
-                            <div style={{ fontSize: '20px', fontWeight: 600, color: '#fff', marginTop: '.5rem' }}>Thit bam, so ca chua</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="card-items">
-                    <img className="card-image-pizza" src={LogoPizza} alt="9DevPizza" />
-                    <div style={{ height: '50%', width: '100%' }}></div>
-                    <div className="card-content">
-                        <div>
-                            <div style={{ fontSize: '20px', fontWeight: 600, color: '#fff' }}>Pizza Thit Bam</div>
-                            <div style={{ fontSize: '20px', fontWeight: 600, color: '#fff', marginTop: '.5rem' }}>Thit bam, so ca chua</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="card-items">
-                    <img className="card-image-pizza" src={LogoPizza} alt="9DevPizza" />
-                    <div style={{ height: '50%', width: '100%' }}></div>
-                    <div className="card-content">
-                        <div>
-                            <div style={{ fontSize: '20px', fontWeight: 600, color: '#fff' }}>Pizza Thit Bam</div>
-                            <div style={{ fontSize: '20px', fontWeight: 600, color: '#fff', marginTop: '.5rem' }}>Thit bam, so ca chua</div>
-                        </div>
-                    </div>
-                </div>
+                <CardPizza title="Pizza Thit Bam" description="Thit bam, so ca chua"/>
             </div>
         </div>
     )
